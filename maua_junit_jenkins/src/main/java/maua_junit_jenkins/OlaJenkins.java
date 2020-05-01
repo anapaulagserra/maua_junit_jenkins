28c567b4fa2023074fe9d85618094222fd9c2df0
Ana <anapaulagserra@gmail.com>
2020-05-01 17:39:01 -0300

    Primeiro Commit

---
+++ maua_junit_jenkins/src/main/java/maua_junit_jenkins/OlaJenkins.java
@@ -0,0 +1,8 @@
+package maua_junit_jenkins;
+
+public class OlaJenkins {
+
+	public String hello() {
+		return "Ola, Jenkins";
+	}
+}
---
+++ maua_junit_jenkins/src/test/java/maua_junit_jenkins/OlaJenkinsTest.java
@@ -0,0 +1,14 @@
+package maua_junit_jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class OlaJenkinsTest {
+	private String resultadoEsperado = "Ola, Jenkins";
+	@Test
+	public void teste () {
+		OlaJenkins instancia = new OlaJenkins();
+		assertEquals(resultadoEsperado, instancia.hello());
+	}
+}
